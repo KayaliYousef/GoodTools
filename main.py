01eb7b38b6156f5adc8aa7ec6248959f15dca53e
Yousef Kayali <kayaliyousef@gmail.com>
2023-06-25 14:23:49 +0200

    :tada: first commit UPLOADING main.py sort.py srt_vvt_converter.py and assets/

---
+++ main.py
@@ -0,0 +1,174 @@
+import difflib
+import glob
+import re
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QTextEdit, QRadioButton, QFileDialog
+from PyQt5.QtGui import QIcon
+
+import sort
+import srt_vtt_converter
+
+class UI(QMainWindow):
+  # constructor
+    def __init__(self):
+        super(UI, self).__init__()
+        uic.loadUi('assets/text_compare.ui', self)
+
+        self.setWindowIcon(QIcon('assets/palmtree.png'))
+        self.setWindowTitle("Compare/Sort/Convert")
+
+        # Define Widgets
+        # Radio Buttons
+        self.srtRadioButton = self.findChild(QRadioButton, "srtRadioButton")
+        self.txtRadioButton = self.findChild(QRadioButton, "txtRadioButton")
+        self.csvRadioButton = self.findChild(QRadioButton, "csvRadioButton")
+        # Push Buttons
+        self.comparePushButton = self.findChild(QPushButton, "comparePushButton")
+        self.sortPushButton = self. findChild(QPushButton, "sortPushButton")
+        self.convertPushButton = self.findChild(QPushButton, "convertPushButton")
+        self.cleanPushButton =self.findChild(QPushButton, "cleanPushButton")
+        self.fileDialog1 =self.findChild(QPushButton, "fileDialog1")    
+        self.fileDialog2 =self.findChild(QPushButton, "fileDialog2")
+        # Entries
+        self.firstFileName = self.findChild(QTextEdit, "firstFile")
+        self.secondFileName = self.findChild(QTextEdit, "secondFile")
+
+        # Connect Buttons
+        self.comparePushButton.clicked.connect(self.compare)
+        self.sortPushButton.clicked.connect(self.sort_srt)
+        self.convertPushButton.clicked.connect(self.convert)
+        self.cleanPushButton.clicked.connect(self.clean)
+        self.fileDialog1.clicked.connect(self.browse1)
+        self.fileDialog2.clicked.connect(self.browse2)
+
+    def browse1(self):
+        self.firstFileName.setText("")
+        fname = QFileDialog.getOpenFileName(self, "choose file", ".", "(*.txt) (*.srt)")
+        self.firstFileName.setText(fname[0])
+    def browse2(self):
+        self.secondFileName.setText("")
+        fname = QFileDialog.getOpenFileName(self, "choose file", ".", "(*.txt) (*.srt)")
+        self.secondFileName.setText(fname[0])
+
+
+    # function to get files in directory
+    def get_files(self, extension:str) -> list[str]:
+        files = []
+        for file in glob.glob("*."+extension):
+            files.append(file)
+        return files
+
+    # function to convert files between srt and vtt
+    def convert(self):
+        srt_files = self.get_files("srt")
+        vtt_files = self.get_files("vtt")
+        for file in srt_files:
+            srt_vtt_converter.convert_srt_to_vtt(file)
+        for file in vtt_files:
+            srt_vtt_converter.convert_vtt_to_srt(file)
+
+    def sort_srt(self):
+        files = sort.get_files()
+        sort.sort(files)
+
+    # function to compare two files (srt or txt) and output the difference in an html file
+    def compare(self):
+        fname1 = self.firstFileName.toPlainText().replace("file:///", "")
+        fname2 = self.secondFileName.toPlainText().replace("file:///", "")
+        if self.srtRadioButton.isChecked():
+            text1 = self.clean_srt(fname1)
+            text2 = self.clean_srt(fname2)
+            # Split the texts into lines
+            lines1 = text1.splitlines()
+            lines2 = text2.splitlines()
+            html_diff = difflib.HtmlDiff().make_file(lines1, lines2)
+            # Make output file name
+            outputName = fname1.replace(".srt", "").replace("\\", "/").split("/")
+            outputName = outputName[-1]
+            # Write the HTML diff to a file
+            with open(outputName+'.html', 'w', encoding="utf-8") as f:
+                f.write(html_diff)
+
+        elif self.txtRadioButton.isChecked():
+            text1 = self.read_txt(fname1)
+            text2 = self.read_txt(fname2)
+            # Split the texts into lines
+            lines1 = text1.splitlines()
+            lines2 = text2.splitlines()
+            html_diff = difflib.HtmlDiff().make_file(lines1, lines2)
+            # Make output file name
+            outputName = fname1.replace(".txt", "").replace("\\", "/").split("/")
+            outputName = outputName[-1]
+            # Write the HTML diff to a file
+            with open(outputName+'.html', 'w', encoding="utf-8") as f:
+                f.write(html_diff)
+
+        elif self.csvRadioButton.isChecked():
+            lines1 = self.read_csv(fname1)
+            lines2 = self.read_csv(fname2)
+            html_diff = difflib.HtmlDiff().make_file(lines1, lines2)
+            # Make output file name
+            outputName = fname1.replace(".csv", "").replace("\\", "/").split("/")
+            outputName = outputName[-1]
+            # Write the HTML diff to a file
+            with open(outputName+'.html', 'w', encoding="utf-8") as f:
+                f.write(html_diff)
+
+
+    def clean(self):
+        srt_files = self.get_files("srt")
+        for file in srt_files:
+            self.srt_to_plaintext(file)
+
+    # function to remove time code and block indexes from srt
+    def clean_srt(self, srt_file_path:str) -> str:
+        path = re.sub(r"\\", "/", srt_file_path)
+        with open(path, 'r', encoding="utf-8") as f:
+            text = f.readlines()
+        # remove time codes and subtitle block indexes
+        cleaned_lines = [line for line in text if not (line.strip().isdigit() or '-->' in line)]
+        # remove empty lines
+        cleaned_lines = [line for line in cleaned_lines if line.strip()]
+        return '\n'.join(cleaned_lines)
+    
+    # function to cenvert srt to plain text removing the line breaks and adding new line breaks after every dot
+    def srt_to_plaintext(self, srt_file_path:str) -> str:
+        path = re.sub(r"\\", "/", srt_file_path)
+        with open(path, 'r', encoding="utf-8") as f:
+            text = f.readlines()
+        # remove time codes and subtitle block indexes
+        cleaned_lines = [line for line in text if not (line.strip().isdigit() or '-->' in line)]
+        # remove empty lines
+        cleaned_lines = [line for line in cleaned_lines if line.strip()]
+        # remove line breaks
+        cleaned_lines = [line.strip() for line in cleaned_lines]
+        # join the text together
+        cleaned_lines = ''.join(cleaned_lines)
+        # add line breaks after dots
+        cleaned_lines = re.sub(r'\.(?![\.\.])\s*', '.\n', cleaned_lines)
+        fileName = srt_file_path.split("/")
+        fileName = fileName[-1].split(".")
+        fileName = fileName[0]
+        with open(f"{fileName}.txt", "w") as vtt_file:
+            vtt_file.write(cleaned_lines)
+    
+    def read_txt(self, txt_file_path:str) -> str:
+        path = re.sub(r"\\", "/", txt_file_path)
+        with open(path, "r", encoding="utf-8") as txt_file:
+            txt = txt_file.read()
+        return txt
+    
+    def read_csv(self, csv_file_path:str) -> list[str]:
+        path = re.sub(r"\\", "/", csv_file_path)
+        with open(path, "r", encoding="utf-8") as csv_file:
+            lines = csv_file.readlines()
+            return lines
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    root = UI()
+    root.show()
+    sys.exit(app.exec_())
---
+++ srt_vtt_converter.py
@@ -0,0 +1,59 @@
+import re
+
+
+def convert_srt_to_vtt(srt_file_path:str) -> None:
+    # Open the SRT file
+    with open(srt_file_path, "r") as srt_file:
+        # Read the contents of the file
+        srt_contents = srt_file.read()
+
+    # Replace "," with "." in time code lines to match VTT timestamp format
+    pattern = r"(\d{2}:\d{2}:\d{2}),(\d{3} --> \d{2}:\d{2}:\d{2}),(\d{3}\n)"
+    replace = r"\1.\2.\3"
+    vtt_contents = re.sub(pattern, replace, srt_contents)
+
+    # Remove translation block numbers
+    vtt_contents = re.sub(r"(?<!.)\d+\n", "", vtt_contents)
+
+    # Add the "WEBVTT" header to the VTT file
+    vtt_contents = "WEBVTT\n\n" + vtt_contents
+
+    # Write the contents to the VTT file
+    fileName = srt_file_path.split("/")
+    fileName = fileName[-1].split(".")
+    fileName = fileName[0]
+    with open(f"{fileName}.vtt", "w") as vtt_file:
+        vtt_file.write(vtt_contents)
+
+
+def convert_vtt_to_srt(srt_file_path:str) -> None:
+    # Open the VTT file
+    with open(srt_file_path, "r") as vtt_file:
+        # Read the contents of the file
+        vtt_contents = vtt_file.read()
+
+    # Remove the "WEBVTT" header from the VTT file
+    vtt_contents = re.sub(r"WEBVTT\n\n", "", vtt_contents)
+
+    # Replace "." with "," in time code lines to match VTT timestamp format
+    pattern = r"(\d{2}:\d{2}:\d{2}).(\d{3} --> \d{2}:\d{2}:\d{2}).(\d{3}\n)"
+    replace = r"\1,\2,\3"
+    vtt_contents = re.sub(pattern, replace, vtt_contents)
+
+    # Add translation block numbers
+    srt_contents = ""
+    block_num = 1
+    for block in vtt_contents.split("\n\n"):
+        if block.strip() == "": # Ignore empty lines
+            continue
+        srt_contents += f"{block_num}\n{block}\n\n"
+        block_num += 1
+
+    srt_contents = srt_contents.strip()
+
+    # Write the contents to the SRT file
+    fileName = srt_file_path.split("/")
+    fileName = fileName[-1].split(".")
+    fileName = fileName[0]
+    with open(f"{fileName}.srt", "w") as srt_file:
+        srt_file.write(srt_contents)
