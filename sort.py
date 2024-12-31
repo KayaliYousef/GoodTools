bcac6f93aa79a0fec8bb1955f7a2dfed9bef8675
Yousef <yousef@ubuntu.fritz.box>
2024-12-31 23:15:14 +0100

    added documentation for the sort function

--- sort.py
+++ sort.py
@@ -1,8 +1,23 @@
 from prep_srt import srt_to_json
 
-def sort(srt_file_path:str, output_file=None, edit_original_file=False):
+def sort(srt_file_path:str, output_file=None, edit_original_file=False) -> None:
+    """
+    Function to sort srt timestamps by time
+
+        Parameters:
+            srt_file_path (str): The path to the srt file 
+            output_file (None or str): If None the sorted srt file will be the old name with `_sorted` appended to the end of the name. If a string were entered, this string will be the name of the sorted file
+            edit_original_file (bool): If True the origial file will be overwritten, else the output will be saved in another file
+
+        Returns:
+            None
+
+    """
+    # Convert the srt file into json
     json_data = srt_to_json(srt_file_path, save_json=False)
+    # Retrieve the list of JSON entries, where each entry represents information for an SRT block. Such as timestamp, block number and text
     entries = json_data.get('entries', [])
+    # Sort the entries by timestamp
     sorted_entries = sorted(entries, key=lambda x: x['time_code'])
     
     block_number = 1
@@ -16,9 +31,11 @@ def sort(srt_file_path:str, output_file=None, edit_original_file=False):
         else:
             fileName = srt_file_path.lower().rsplit("/", 1)[-1].rsplit(".", 1)[0] + "_sorted.srt"
 
+    # Open the output file
     with open(fileName, "w", encoding="utf-8") as f:
+        # Iterate over the sorted entries
         for entry in sorted_entries:
-
+            # Get the timestamp and text from this entry
             time_code = entry.get('time_code', '')
             text = entry.get('text', '')
 
