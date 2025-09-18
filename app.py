28efe6bf1403528b6b221f9116b96412f7ccf4b2
Yousef Kayali <kayaliyousef@gmail.com>
2025-09-18 20:54:13 +0200

    Finished the web app

--- app.py
+++ app.py
@@ -10,6 +10,15 @@ import io
 import srt_vtt_converter
 import re
 import correct_intersected_srt
+import sync_srt
+import prep_srt
+import zipfile
+
+PUNCTUATION_LIST = [".", ",", "?", ":", "!"]
+MAX_CHAR_PER_LINE_NORMAL = 42
+MIN_CHAR_PER_LINE_NORMAL = 30
+MAX_CHAR_PER_LINE_SHORT = 30
+MIN_CHAR_PER_LINE_SHORT = 20
 
 app = Flask(__name__)
 
@@ -83,10 +92,12 @@ def compare():
 def process_srt():
 
     if request.method == "POST":
+        app.logger.info("POST request detected")
         srt_file = request.files['srt-file']
+        app.logger.info("Pulled the uploaded file")
         try:
             srt_file_name = secure_filename(srt_file.filename)
-
+            app.logger.info("Secured file name")
             srt_file_extension = srt_file_name.rsplit(".", 1)[-1]
 
             if srt_file_extension.lower() != "srt" and srt_file_extension.lower() != "vtt":
@@ -94,12 +105,14 @@ def process_srt():
                 return redirect(request.url)
             
             srt_file_path = os.path.join(app.config['UPLOAD_FOLDER'], srt_file_name)
+            app.logger.info("Created file path")
             srt_file.save(srt_file_path)
+            app.logger.info("File was successfully uploaded")
 
             if 'action' in request.form:
 
                 if request.form['action'] == 'sort':
-
+                    app.logger.info("Sort button pressed")
                     if srt_file_extension.lower() == "srt":
                         sort.sort(srt_file_path)
                         output_extension = "_sorted.srt"
@@ -108,7 +121,7 @@ def process_srt():
                         flash('This function only accepts SRT files', 'info')
 
                 elif request.form['action'] == 'convert-srt-vtt':
-
+                    app.logger.info("Convert SRT/VTT button pressed")
                     if srt_file_extension.lower() == "srt":
                         srt_vtt_converter.convert_srt_to_vtt(srt_file_path)
                         output_extension = ".vtt"
@@ -121,7 +134,7 @@ def process_srt():
                         flash('This function only accepts SRT or VTT files', 'info')
 
                 elif request.form['action'] == "clean-srt":
-
+                    app.logger.info("Clean SRT button pressed")
                     if srt_file_extension.lower() == "srt":
                         hf.srt_to_plaintext(srt_file_path)
                         output_extension = ".txt" 
@@ -130,7 +143,7 @@ def process_srt():
                         flash('This function only accepts SRT files', 'info')
 
                 elif request.form['action'] == "check-srt-sequence":
-
+                    app.logger.info("Check SRT sequence button pressed")
                     if srt_file_extension.lower() == "srt":
                         error_within_one_block = []
                         error_between_two_blocks = []
@@ -259,25 +272,44 @@ def process_srt():
                         flash('This function only accepts SRT files', 'info')
                     
                 elif request.form['action'] == "sync":
+                    app.logger.info("Sync button pressed")
                     if srt_file_extension.lower() == "srt":
+                        app.logger.info("File type was validated")
+                        punctuations = PUNCTUATION_LIST
+                        if request.form.get("split-at-punctuation"):
+                            split_at_punctuation = True
+                        else:
+                            split_at_punctuation = False
 
                         if request.form['sync-option'] == "Normal":
-                            print("First Case")
+                            app.logger.info("Sync-option 'Normal' was chosen")
+                            max_char_per_line = MAX_CHAR_PER_LINE_NORMAL
+                            min_char_per_line = MIN_CHAR_PER_LINE_NORMAL
+            
                         elif request.form['sync-option'] == "Short":
-                            print("Second case")
+                            app.logger.info("Sync-option 'Short' was chosen")
+                            max_char_per_line = MAX_CHAR_PER_LINE_SHORT
+                            min_char_per_line = MIN_CHAR_PER_LINE_SHORT
                         else:
-                            print("Third case")
-                            print(request.form.get("min-input"))
-                            print(request.form.get("max-input"))
-                        os.remove(srt_file_path)
-                        return redirect(request.url)
+                            app.logger.info("Sync-option 'Custom' was chosen")
+                            max_char_per_line = int(request.form.get("max-input"))
+                            min_char_per_line = int(request.form.get("min-input"))
+                            if min_char_per_line > max_char_per_line:
+                                min_char_per_line, max_char_per_line = max_char_per_line, min_char_per_line
+                        sort.sort(srt_file_path, edit_original_file=True)
+                        app.logger.info("File was Sorted successfully")
+                        hf.clean_extra_white_spaces(srt_file_path)
+                        app.logger.info("Whitespaces were successfully removed")
+                        correct_intersected_srt.correct_intersected_blocks(srt_file_path)
+                        app.logger.info("Intersected translation blocks were corrected successfully")
+                        sync_srt.sync(srt_file_path, max_char_per_line, min_char_per_line, split_at_punctuation, punctuations, srt_file_path)
+                        app.logger.info("File was successfully synchronized")
+                        output_extension = ".srt"
 
                 
-
-
                 # This file is being created by other functions like (sort, convert between srt/vtt and clean)
                 tempfile_path = srt_file_path.rsplit("/", 1)[-1].rsplit(".", 1)[0] + output_extension
-                print(tempfile_path)
+
                 # Read file into memory
                 with open(tempfile_path, "rb") as f:
                     file_bytes = io.BytesIO(f.read())
@@ -303,8 +335,140 @@ def process_srt():
         
     return render_template('process_srt.html')
 
-@app.route('/srt_prep')
+@app.route('/srt_prep', methods=['GET', 'POST'])
 def srt_prep():
+
+    if request.method == "POST":
+        try:
+            if "action" in request.form:
+
+                if request.form["action"] == "prep-srt":
+                    srt_file = request.files['srt-file']
+                    srt_file_name = secure_filename(srt_file.filename)
+                    srt_file_extension = srt_file_name.rsplit(".", 1)[-1]
+                    srt_file_path = os.path.join(app.config['UPLOAD_FOLDER'], srt_file_name)
+                    srt_file.save(srt_file_path)
+
+                    if srt_file_extension == "srt":
+                        hf.sub_srt_codes(srt_file_path, save_output_where_input_is_located=True)
+                        prep_srt.srt_to_json(srt_file_path)
+
+                        text_file_path = srt_file_path.rsplit("/", 1)[-1].rsplit(".", 1)[0] + ".txt"
+                        json_file_path = srt_file_path.rsplit("/", 1)[-1].rsplit(".", 1)[0] + "_output.json"
+
+                        # Create an in-memory ZIP file
+                        zip_buffer = io.BytesIO()
+                        with zipfile.ZipFile(zip_buffer, "w", zipfile.ZIP_DEFLATED) as zf:
+                            # Add text file
+                            with open(text_file_path, "rb") as f:
+                                zf.writestr("output.txt", f.read())
+                            
+                            # Add json file
+                            with open(json_file_path, "rb") as f:
+                                zf.writestr("output.json", f.read())
+
+                        # Go back to the beginning of the BytesIO buffer
+                        zip_buffer.seek(0)
+
+                        try:
+                            os.remove(srt_file_path)
+                        except Exception as e:
+                            app.logger.error(f"Error deleting srt file {srt_file_path}: {e}")
+                        
+                        try:
+                            os.remove(text_file_path)
+                        except Exception as e:
+                            app.logger.error(f"Error deleting text file {text_file_path}: {e}")
+
+                        try:
+                            os.remove(json_file_path)
+                        except Exception as e:
+                            app.logger.error(f"Error deleting json file {json_file_path}: {e}")
+
+                        # Send zip file to client
+                        return send_file(
+                            zip_buffer,
+                            as_attachment=True,
+                            download_name="files.zip",
+                            mimetype="application/zip"
+                        )
+
+                    else:
+                        flash('This function only accepts SRT files', 'info')
+
+                else:
+
+                    punctuations = PUNCTUATION_LIST
+                    if request.form.get("split-at-punctuation"):
+                        split_at_punctuation = True
+                    else:
+                        split_at_punctuation = False
+
+                    if request.form['sync-option'] == "Normal":
+                        app.logger.info("Sync-option 'Normal' was chosen")
+                        max_char_per_line = MAX_CHAR_PER_LINE_NORMAL
+                        min_char_per_line = MIN_CHAR_PER_LINE_NORMAL
+        
+                    elif request.form['sync-option'] == "Short":
+                        app.logger.info("Sync-option 'Short' was chosen")
+                        max_char_per_line = MAX_CHAR_PER_LINE_SHORT
+                        min_char_per_line = MIN_CHAR_PER_LINE_SHORT
+                    else:
+                        app.logger.info("Sync-option 'Custom' was chosen")
+                        max_char_per_line = int(request.form.get("max-input"))
+                        min_char_per_line = int(request.form.get("min-input"))
+                        if min_char_per_line > max_char_per_line:
+                            min_char_per_line, max_char_per_line = max_char_per_line, min_char_per_line
+
+                    json_file = request.files['json-file']
+                    json_file_name = secure_filename(json_file.filename)
+                    json_file_extension = json_file_name.rsplit(".", 1)[-1]
+                    json_file_path = os.path.join(app.config['UPLOAD_FOLDER'], json_file_name)
+                    json_file.save(json_file_path)
+
+                    text_file = request.files['text-file']
+                    text_file_name = secure_filename(text_file.filename)
+                    text_file_extension = text_file_name.rsplit(".", 1)[-1]
+                    text_file_path = os.path.join(app.config['UPLOAD_FOLDER'], text_file_name)
+                    text_file.save(text_file_path)
+
+                    prep_srt.reconstruct_srt_from_json_and_txt(json_file_path, text_file_path)
+
+                    reconstructed_file_path = text_file_path.rsplit(".", 1)[0]+"_new.srt"
+                    sync_srt.sync(reconstructed_file_path, max_char_per_line, min_char_per_line, split_at_punctuation, punctuations, reconstructed_file_path)
+
+                    with open(reconstructed_file_path, "rb") as f:
+                        file_bytes = io.BytesIO(f.read())
+
+                    try:
+                        os.remove(json_file_path)
+                    except Exception as e:
+                        app.logger.error(f"Error deleting json file {json_file_path}: {e}")
+
+                    try:
+                        os.remove(text_file_path)
+                    except Exception as e:
+                        app.logger.error(f"Error deleting text file {text_file_path}: {e}")
+
+                    try:
+                        os.remove(reconstructed_file_path)
+                    except Exception as e:
+                        app.logger.error(f"Error deleting reconstructed srt file {reconstructed_file_path}: {e}")
+
+                    # Send file for download
+                    return send_file(
+                        file_bytes,
+                        as_attachment=True,
+                        download_name=f'output.srt',
+                        mimetype='text/plain'
+                    )
+
+
+        except Exception as e:
+            flash(f'Error processing file: {str(e)}', 'error')
+            return redirect(request.url)
+
+        
     return render_template('srt_prep.html')
 
 if __name__ == '__main__':
